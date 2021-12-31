84d156f7a3ecf15130669b2554aae715cb6e9937
Alexander Rabenstein <alexander@die-rabensteins.de>
2021-12-31 14:47:51 +0100

    add .home subdomain

--- provision/terraform/cloudflare/main.tf
+++ provision/terraform/cloudflare/main.tf
@@ -83,7 +83,7 @@ data "http" "ipv4" {
 }
 
 resource "cloudflare_record" "ipv4" {
-  name    = "ipv4"
+  name    = "ipv4home"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
   value   = chomp(data.http.ipv4.body)
   proxied = true
@@ -92,9 +92,9 @@ resource "cloudflare_record" "ipv4" {
 }
 
 resource "cloudflare_record" "root" {
-  name    = data.sops_file.cloudflare_secrets.data["cloudflare_domain"]
+  name    = "home.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  value   = "ipv4home.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = true
   type    = "CNAME"
   ttl     = 1
@@ -103,7 +103,7 @@ resource "cloudflare_record" "root" {
 resource "cloudflare_record" "hajimari" {
   name    = "hajimari"
   zone_id = lookup(data.cloudflare_zones.domain.zones[0], "id")
-  value   = "ipv4.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
+  value   = "home.${data.sops_file.cloudflare_secrets.data["cloudflare_domain"]}"
   proxied = true
   type    = "CNAME"
   ttl     = 1
